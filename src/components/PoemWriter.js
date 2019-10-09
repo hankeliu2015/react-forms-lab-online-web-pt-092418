ff1e596339418e34a2f134a3cf9a30ac7a4dc0eb
github username <github email address>
2019-10-09 12:20:02 -0400

    create a tenery condition to mute or display a division which contains the validation message

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
 class PoemWriter extends Component {
+  state = {
+    poem: '',
+    valid: false
+  }
+
+
+
   render() {
     return (
       <div>
-        <textarea rows="5" cols="50" />
-        <div id="poem-validation-error" style={{color: "red"}}>
+        <textarea value={this.state.poem} onChange={this.handleChange} rows="5" cols="50" />
+
+        {this.state.valid ? null : <div id="poem-validation-error" style={{color: "red"}} >
           This poem is not written in the right structure!
-        </div>
+        </div>}
 
       </div>
     )
